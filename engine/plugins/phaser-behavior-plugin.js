ea1cea2b96124d4d42c7a32fd17433956131b2dc
Oli4242 <Oli4242@users.noreply.github.com>
2017-06-16 20:39:32 +0200

    update Phaser Behavior Plugin to 1.1.0

--- engine/plugins/phaser-behavior-plugin.js
+++ engine/plugins/phaser-behavior-plugin.js
@@ -1,237 +1,319 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.BehaviorPlugin = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/* global Phaser */
-'use strict'
+'use strict';
 
-var BehaviorSystem = require('behavior-system')
+var BehaviorSystem = require('behavior-system');
+var NULL = undefined;
 
-var plugin = function (game, parent) {
-  Phaser.Plugin.call(this, game, parent)
-  this._system = new BehaviorSystem()
-}
+var Plugin = {
 
-plugin.prototype = Object.create(Phaser.Plugin)
+  VERSION: '1.1.0',
 
-plugin.prototype.enable = function (gameObject) {
-  this._system.enable(gameObject)
-}
+  init: function () {
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-plugin.prototype.disable = function (gameObject) {
-  this._system.disable(gameObject)
-}
+    this._system = new BehaviorSystem();
+    this._filter = config.filter;
+  },
+  enable: function (gameObject) {
+    var filter = this._filter;
+    var enabled = false;
 
-plugin.prototype.preUpdate = function () {
-  this._system.processAll('preUpdate')
-}
+    if (filter !== NULL) {
+      if (filter(gameObject) === true) {
+        enabled = this._system.enable(gameObject);
+      }
+    } else {
+      enabled = this._system.enable(gameObject);
+    }
 
-plugin.prototype.update = function () {
-  this._system.processAll('update')
-}
+    if (enabled === true && gameObject.events !== NULL && gameObject.events.onDestroy !== NULL) {
+      gameObject.events.onDestroy.add(this._onDestroyCallback, this._system);
+    }
 
-plugin.prototype.render = function () {
-  this._system.processAll('render')
-}
+    return enabled;
+  },
+  disable: function (gameObject) {
+    var removeListener = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
 
-plugin.prototype.postRender = function () {
-  this._system.processAll('postRender')
-}
+    var disabled = this._system.disable(gameObject);
+    if (disabled === true && removeListener === true) {
+      if (gameObject.events !== NULL && gameObject.events.onDestroy !== NULL) {
+        gameObject.events.onDestroy.remove(this._onDestroyCallback, this._system);
+      }
+    }
+    return disabled;
+  },
+  preUpdate: function () {
+    this._system.globalProcessAll('preUpdate', this.game, this.game.time.physicsElapsed);
+  },
+  update: function () {
+    this._system.globalProcessAll('update', this.game, this.game.time.physicsElapsed);
+    this._system.globalProcessAll('postUpdate', this.game, this.game.time.physicsElapsed);
+  },
+  render: function () {
+    this._system.globalProcessAll('preRender', this.game);
+    this._system.globalProcessAll('render', this.game);
+  },
+  postRender: function () {
+    this._system.globalProcessAll('postRender', this.game);
+  },
+  _onDestroyCallback: function (gameObject) {
+    this.disable(gameObject, false);
+  }
+};
 
-if (window.Phaser) window.Phaser.Plugin.Behavior = plugin
+if (window.Phaser) window.Phaser.Plugin.Behavior = Plugin;
 
-module.exports = plugin
-
+module.exports = Plugin;
 },{"behavior-system":3}],2:[function(require,module,exports){
-var NULL // never use "null"
-var extend = require('./helper/extend')
-var Recycler = require('./helper/recycler')
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-function BehaviorContainer (gameObject) {
-  this._constructor(gameObject)
+var NULL = undefined; // never use "null"
+var extend = require('./helper/extend');
+var removeByIndex = require('./helper/remove-by-index');
+
+function BehaviorContainer(gameObject, parent) {
+  this.gameObject = gameObject;
+
+  // store instances
+  this._behaviorList = [];
+
+  // use the 'key' to store the index of the instances
+  this._behaviorMap = {};
+
+  // store paused instances
+  this._pausedBehaviors = {};
+
+  this.parent = parent;
 }
 
 BehaviorContainer.prototype = {
 
-  _constructor: function (gameObject) {
-    this.gameObject = gameObject
-    this._behaviorList = new Recycler() // store instances
-    this._behaviorMap = {} // store indexes
-    return this
-  },
+  constructor: BehaviorContainer,
+
+  index: NULL, // array index on system (defined by the system)
 
   set: function (key, behavior, options) {
-    if (this.has(key)) throw new Error('"' + key + '" already in use')
-    else if (behavior === NULL) throw new Error('invalid behavior')
+    if (key.length === 0 || this.has(key)) return false;
 
-    var ref = Object.create(behavior)
-    var defaults = extend({}, behavior.options)
+    var ref = Object.create(behavior);
+    if (behavior.options !== NULL) {
+      ref.options = extend(extend({}, behavior.options), options);
+    } else {
+      ref.options = extend({}, options);
+    }
+    ref.options.$key = key;
+    this._behaviorMap[key] = this._behaviorList.push(ref) - 1;
 
-    ref.options = extend(defaults, options)
-    ref.options.$key = key
+    if (ref.create !== NULL) this.process(key, 'create'
 
-    this._behaviorMap[key] = this._behaviorList.getNextIndex()
-    this._behaviorList.push(ref)
-
-    if (ref.create !== NULL) ref.create(this.gameObject, ref.options)
-
-    return ref
+    // returns the key of newly created behavior instance
+    );return key;
   },
-
   remove: function (key) {
-    var index = this._behaviorMap[key]
-    if (index >= 0) {
-      var ref = this._behaviorList.iterable[index]
-      if (ref.destroy !== NULL) ref.destroy(this.gameObject, ref.options)
-      this._behaviorMap[key] = NULL
-      this._behaviorList.remove(index)
+    if (!this.has(key)) return false;
+
+    var index = this._behaviorMap[key]; // array index of the key
+    var list = this._behaviorList;
+    var len = list.length;
+    var ref = list[index];
+    var last = list[list.length - 1];
+
+    // call method 'destroy' before
+    if (ref.destroy !== NULL) this.process(key, 'destroy');
+
+    this._behaviorMap[key] = NULL;
+    if (len !== 1 && index + 1 !== len) {
+      this._behaviorMap[last.options.$key] = index;
+    }
+    removeByIndex(this._behaviorList, index
+
+    // remove from paused list if necessary
+    );this._pausedBehaviors[key] = NULL;
+
+    return true;
+  },
+  removeAll: function () {
+    var list = this._behaviorList;
+    var len = list.length;
+
+    if (len === 0) return;
+
+    for (i = len - 1; i !== -1; --i) {
+      this.remove(list[i].options.$key);
     }
   },
-
   has: function (key) {
-    return this._behaviorMap[key] !== NULL
+    return this._behaviorMap[key] !== NULL;
   },
-
-  get: function (key) {
-    var index = this._behaviorMap[key]
-    return index >= 0 ? this._behaviorList.iterable[index] : NULL
+  pause: function (key) {
+    if (this.has(key) && !this.isPaused(key)) {
+      // process `paused` before pause the instance
+      this.process(key, 'paused');
+      this._pausedBehaviors[key] = true;
+      return true;
+    }
+    return false;
   },
+  pauseAll: function () {
+    var list = this._behaviorList;
+    var len = list.length;
 
-  process: function (methodName) {
-    var i = 0
-    var list = this._behaviorList.iterable
-    var len = list.length
-    for (; i < len; i++) {
-      var ref = list[i]
-      if (ref !== NULL && typeof ref[methodName] === 'function') {
-        ref[methodName](this.gameObject, ref.options)
+    if (len === 0) return;
+
+    for (var _i = 0; _i < len; ++_i) {
+      this.pause(list[_i].options.$key);
+    }
+  },
+  resume: function (key) {
+    if (this.has(key) && this.isPaused(key)) {
+      this._pausedBehaviors[key] = NULL;
+      // process `resumed` after resume the instance
+      this.process(key, 'resumed');
+      return true;
+    }
+    return false;
+  },
+  resumeAll: function () {
+    var list = this._behaviorList;
+    var len = list.length;
+
+    if (len === 0) return;
+
+    for (var _i2 = 0; _i2 < len; ++_i2) {
+      this.resume(list[_i2].options.$key);
+    }
+  },
+  isPaused: function (key) {
+    return this._pausedBehaviors[key] !== NULL;
+  },
+  process: function (key, methodName) {
+    if (!this.has(key) || this.isPaused(key)) return;
+
+    var index = this._behaviorMap[key];
+    var ref = this._behaviorList[index];
+    var method = ref[methodName];
+    var gameObject = this.gameObject;
+    var options = ref.options;
+    var result = NULL;
+
+    if (method !== NULL) {
+      for (var _len = arguments.length, args = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+        args[_key - 2] = arguments[_key];
+      }
+
+      switch (args.length) {
+        case 0:
+          result = method(gameObject, options);
+          break;
+        case 1:
+          result = method(gameObject, options, args[0]);
+          break;
+        case 2:
+          result = method(gameObject, options, args[0], args[1]);
+          break;
+        case 3:
+          result = method(gameObject, options, args[0], args[1], args[2]);
+          break;
+        default:
+          result = method.apply(NULL, [gameObject, options].concat(_toConsumableArray(args)));
+          break;
       }
     }
-    return this
+
+    return result;
+  },
+  processAll: function (methodName) {
+    var list = this._behaviorList;
+    var len = list.length;
+
+    if (len === 0) return;
+
+    for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+      args[_key2 - 1] = arguments[_key2];
+    }
+
+    for (var _i3 = 0; _i3 < len; ++_i3) {
+      this.process.apply(this, [list[_i3].options.$key, methodName].concat(_toConsumableArray(args)));
+    }
   }
+};
 
-}
+module.exports = BehaviorContainer;
+},{"./helper/extend":4,"./helper/remove-by-index":5}],3:[function(require,module,exports){
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-module.exports = BehaviorContainer
+var NULL = undefined; // never use "null"
+var BehaviorContainer = require('./behavior-container');
+var removeByIndex = require('./helper/remove-by-index');
 
-},{"./helper/extend":4,"./helper/recycler":5}],3:[function(require,module,exports){
-var NULL // never use "null"
-var BehaviorContainer = require('./behavior-container')
-var Recycler = require('./helper/recycler')
-
-function BehaviorSystem () {
-  this._constructor()
+function BehaviorSystem() {
+  this._children = [];
 }
 
 BehaviorSystem.prototype = {
 
-  _constructor: function () {
-    this._children = new Recycler()
-    return this
-  },
+  constructor: BehaviorSystem,
 
   enable: function (gameObject) {
     if (gameObject.behaviors === NULL) {
-      var container = new BehaviorContainer(gameObject)
-      var index = this._children.getNextIndex()
-      container.id = index
-      this._children.push(container)
-      gameObject.behaviors = container
+      gameObject.behaviors = new BehaviorContainer(gameObject, this);
+      gameObject.behaviors.index = this._children.push(gameObject.behaviors) - 1;
+      return true;
     }
-    return gameObject
+    return false;
   },
-
   disable: function (gameObject) {
-    if (gameObject.behaviors !== NULL) {
-      this._children.remove(gameObject.behaviors.id)
-      gameObject.behaviors = NULL
+    if (gameObject.behaviors !== NULL && gameObject.behaviors.parent === this) {
+      gameObject.behaviors.removeAll();
+      removeByIndex(this._children, gameObject.behaviors.index);
+      gameObject.behaviors = NULL;
+      return true;
     }
-    return gameObject
+    return false;
   },
+  globalProcessAll: function (methodName) {
+    var list = this._children;
+    var len = list.length;
 
-  processAll: function (methodName) {
-    var i = 0
-    var list = this._children.iterable
-    var len = list.length
-    for (; i < len; i++) {
-      var child = list[i]
-      if (child !== NULL) child.process(methodName)
+    if (len === 0) return;
+
+    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      args[_key - 1] = arguments[_key];
+    }
+
+    for (var i = 0; i < len; i++) {
+      var _list$i;
+
+      (_list$i = list[i]).processAll.apply(_list$i, [methodName].concat(_toConsumableArray(args)));
     }
   }
-}
+};
 
-module.exports = BehaviorSystem
-
-},{"./behavior-container":2,"./helper/recycler":5}],4:[function(require,module,exports){
-function extend (dest, source) {
+module.exports = BehaviorSystem;
+},{"./behavior-container":2,"./helper/remove-by-index":5}],4:[function(require,module,exports){
+function extend(dest, source) {
   for (var key in source) {
     if (source.hasOwnProperty(key)) {
-      dest[key] = source[key]
+      dest[key] = source[key];
     }
   }
-  return dest
+  return dest;
 }
 
-module.exports = extend
-
+module.exports = extend;
 },{}],5:[function(require,module,exports){
-var NULL // never use "null"
+// remove the element at 'index' and put the last element of array in the 'index' (if array.length > 1)
+function removeByIndex(array, index) {
+  var size = array.length;
+  if (size === 0 || index >= size || index < 0) return;else if (size === 1 || index + 1 === size) return array.pop();
 
-/*
- * Used to track avaliable empry spaces in an array
-*/
-function Recycler (arr) {
-  this._constructor(arr)
+  var value = array[index];
+  array[index] = array.pop();
+
+  return value;
 }
 
-Recycler.prototype = {
-
-  _constructor: function (arr) {
-    this.iterable = Array.isArray(arr) ? arr.slice() : []
-    this._recycledIndexes = []
-    return this
-  },
-
-  push: function (value) {
-    if (value === NULL) return new TypeError('undefined value')
-    this._set(value)
-    return this.iterable.length
-  },
-
-  remove: function (index) {
-    if (index === NULL || index >= this.iterable.length || index < 0) throw new Error('invalid index')
-    var value
-
-    if (index === (this.iterable.length - 1)) {
-      value = this.pop()
-    } else {
-      value = this.iterable[index]
-      this.iterable[index] = NULL
-      this._recycleIndex(index)
-    }
-
-    return value
-  },
-
-  pop: function () {
-    return this.iterable.pop()
-  },
-
-  getNextIndex: function () {
-    var recycled = this._recycledIndexes
-    var len = recycled.length
-    return len > 0 ? recycled[len - 1] : this.iterable.length
-  },
-
-  _set: function (value) {
-    var index = this._recycledIndexes.pop()
-    if (index === NULL) index = this.iterable.length
-    this.iterable[index] = value
-  },
-
-  _recycleIndex: function (index) {
-    this._recycledIndexes.push(index)
-  }
-}
-
-module.exports = Recycler
-
+module.exports = removeByIndex;
 },{}]},{},[1])(1)
 });
